35620d8a933898fee68d24dd5e7e6b1011deaad6
Marcio Marchiori <80441475+Marcio-Marchiori@users.noreply.github.com>
2022-04-03 23:06:07 -0300

    2h in

--- main.go
+++ main.go
@@ -3,19 +3,42 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
+	var conferenceName string = "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("Welcome to %v mate!\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get the tickets here.")
 
+	var bookings []string
+
 	var userName string
-	var userTickets int
+	var lastName string
+	var email string
+	var userTickets uint
+
 	// ask user for input
+	fmt.Println("Enter your first name: ")
 	fmt.Scan(&userName)
 
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter the number of tickets you want to purchase: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, userName+" "+lastName)
+
+	fmt.Printf("The whole slice: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Slice type: %T\n", bookings)
+	fmt.Printf("Slice length: %v\n", len(bookings))
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", userName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
